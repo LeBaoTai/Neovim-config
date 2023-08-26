013f869f6fafdd34a101557a3dfa0faaef9a19ae
Le Bao Tai <fnight644@gmail.com>
2023-08-26 11:15:24 +0700

    add neovide setting

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+  vim.o.guifont = "FiraCode Nerd Font:h11.7" -- text below applies for VimScriptj
+  -- cursor
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_vfx_particle_phase = 1.5
+  vim.g.neovide_cursor_vfx_particle_curl = 1.0
+  vim.g.neovide_cursor_vfx_particle_density = 9.0
+  vim.g.neovide_cursor_vfx_particle_lifetime = 2.0
+  vim.g.neovide_cursor_vfx_opacity = 400.0
+end
